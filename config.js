e7c690b83987f865f5db33c92edcbfb5944651d3
yishan-hung <119563070+yishan-hung@users.noreply.github.com>
2023-09-05 01:37:41 +1000

    Update config.js

--- config.js
+++ config.js
@@ -42,7 +42,7 @@ var config = {
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "开始",
+        turn_on: "Let's start!",
         play: "Music",
         bannar_coming: "Color",
         balloons_flying: "Seems like something still missing...",
