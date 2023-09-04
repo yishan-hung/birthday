171ed60ab0fafa885d7ab841ea61715a5726da22
yishan-hung <119563070+yishan-hung@users.noreply.github.com>
2023-09-05 01:18:40 +1000

    Update config.js

--- config.js
+++ config.js
@@ -3,17 +3,29 @@ var config = {
     // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
     // 每句话尽量不要超过15个字,不然展示效果可能不太好
     texts: [
-        "送给我",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
-        "心爱的小可爱",  // 同上...
+        "送給我",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+        "最可愛美麗漂亮的女高中生",  // 同上...
         "今天是你的生日",
-        "这是我们在一起的",
-        "第三个生日了哦",
-        "去年的生日",
-        "还记得在王婆串串吃的呢",
-        "今年要吃好的喽哦",
-        "要把我家可爱猪猪喂饱饱",
-        "然后抱走",
-        "YAMI~~",
+        "雖然去年才認識",
+        "但沒想到跨越了距離、年紀",
+        "我們變得超好的",
+        "這一年經歷了很多",
+        "變成某人的還變成美麗小女人",
+        "知道你實驗、課業啥都很煩心",
+        "雖然沒辦法時刻陪在你身邊",
+        "但無論如何，我會用我的臂膀",
+        "全力保護著你",
+        "雖然可能是請教授們保護啦...",
+        "但我會盡力的",
+        "最喜歡你的笑容所以",
+        "壓力再大也不要失去對世界的純真",
+        "不要為了追趕而失去自己",
+        "受不了時就狂吃吧! 外表真的還好",
+        "反正你在我眼裡怎樣都閃閃發光",
+        "You will shine in your own way",
+        "So live a life you will remember",
+        "Best wishes and best 17 years old."
+        "Love you my bestie ;)"
     ],
     /**
      * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
@@ -25,18 +37,18 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        "心爱的小可爱": "./imgs/xiaokeai.png",
+        "最可愛美麗漂亮的女高中生": "./imgs/Love.jpg",
         // "今天是你的生日": "./imgs/birthday.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
         turn_on: "开始",
-        play: "音乐",
-        bannar_coming: "颜色",
-        balloons_flying: "好像少点东西",
-        cake_fadein: "蛋糕？",
-        light_candle: "蜡烛？",
-        wish_message: "生日快乐",
-        story: "A MESSAGE FOR YOU",
+        play: "Music",
+        bannar_coming: "Color",
+        balloons_flying: "Seems like something still missing...",
+        cake_fadein: "A huge cake?",
+        light_candle: "17 candles？",
+        wish_message: "Happy 17 years old (love heart)",
+        story: "If you need me, I'll always be there",
     }
 };
